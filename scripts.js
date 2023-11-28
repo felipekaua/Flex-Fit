50205da6e067ad0a4f716c1ecf9b530a45831939
Felipe Kauã de Lima <fkauadelima@gmail.com>
2023-11-28 19:34:59 -0300

    [add]Parâmetros da entrega 2

--- scripts.js
+++ scripts.js
@@ -14,6 +14,95 @@ function navigateToHome(){
     location.assign("./index.html"); 
 }
 
+function handleSubmit(){
+    const tip = document.getElementById("tip").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const cidade = document.getElementById("cidade").value;
+    const estado = document.getElementById("estado").value;
+    let counter = localStorage.getItem("counter");
+    if(counter=="" || counter==undefined || counter==null){
+        counter=0;
+    }
+    if(tip!="" && name!="" && email!="" && cidade!="" && estado!=""){
+    counter++;
+    localStorage.setItem("counter", counter);
+    localStorage.setItem("tip"+"-"+counter, tip+"-"+name+"-"+email+"-"+cidade+"-"+estado);
+    }
+}
+
+function loadTips(){
+    document.getElementById("list").innerHTML="";
+    const counter = localStorage.getItem("counter");
+    for(let i=1; i<=counter; i++){
+        const tipString = localStorage.getItem("tip"+"-"+i);
+        if(tipString!=null){
+            const array = tipString.split("-");
+            const tip = array[0];
+            const name = array[1];
+            const email = array[2]; 
+            const cidade = array[3]; 
+            const estado = array[4]; 
+        
+            document.getElementById("list").innerHTML = 
+            `
+            ${document.getElementById("list").innerHTML}
+            <div>
+            <li>${tip} <img class="delete" id="${"tip"+"-"+i}" src="./Assets/x.png"></li>
+            <p>${name}(${email}) - ${cidade},${estado}</p>
+            <div/>
+            `;
+        }
+    }
+}
+
+const buttons = document.querySelectorAll('.delete');
+
+document.body.addEventListener('click', function (evt) {
+    if (evt.target.className === 'delete') {
+        let tipId = evt.target.id;
+        localStorage.removeItem(tipId);
+        location.reload();
+    }
+}, false);
+
+
+function deleteTip(){
+    localStorage.removeItem(tip);
+}
+
+window.onload = function() {
+    if (window.location.href.match('dicas.html') != null) {
+    loadTips();
+    }
+}
+
+function handleClear(){
+    document.getElementById("tip").value="";
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("cidade").value="";
+    document.getElementById("estado").value="";
+}
+
+function handleClearAll(){
+    localStorage.clear(); 
+    location.reload();
+}
+
+function filtre(){
+    loadTips();
+    const term = document.getElementById("filter").value;
+    const lista = document.getElementById("list");
+    const array = lista.children;
+    let contains = false;
+    for(let i=0; i<array.length;i++){
+        contains = array[i].innerHTML.includes(term);
+        if(!contains){
+            array[i].style.display="none";
+        }
+    }
+}
 // components
 
 header.innerHTML = 
@@ -31,6 +120,7 @@ header.innerHTML =
             <a href="./estatico.html">Estático</a>
             <a href="./dinamico.html">Dinâmico</a>
         </div>
+        <a href="./dicas.html">Dicas</a>
         <a href="./sobre.html">Sobre</a>
     </nav>
     <div onclick="openMenu()" class="closeNavBar"></div>
